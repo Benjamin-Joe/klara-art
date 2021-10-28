27baf37149e9c842beb575cadab0613f32c8ee59
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-10-28 22:32:32 +0000

    Add css/media folders and ability to access them in settings.py and urls.py

--- klarart/urls.py
+++ klarart/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Urls.py main file
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
