9a28d33390c8225597c7a3896a37ba62edf56454
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-01 20:26:12 +0000

    Create contact page and about page and add to navbar

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ from . import views
 # Urls.py home file
 
 urlpatterns = [
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('contact/', views.contact_view, name='contact'),
+    path('about/', views.about_view, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Homepage view
 def home_view(request):
     return render(request, 'home/home.html')
+
+
+# Contact view
+def contact_view(request):
+    return render(request, 'contact/contact.html')
+
+
+# About view
+def about_view(request):
+    return render(request, 'about/about.html')
+
+
