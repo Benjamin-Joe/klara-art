dfd6a78677695a04f4f1e9323faeae0d7fff0347
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-01 10:44:48 +0000

    Align the footer content

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Category Model
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, default='test')
 
     class Meta:
         verbose_name_plural = "categories"
@@ -17,6 +17,7 @@ class Category(models.Model):
 class Products(models.Model):
     category = models.ForeignKey('Category', related_name='product', null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, default='001')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images/')
     description = models.TextField()
