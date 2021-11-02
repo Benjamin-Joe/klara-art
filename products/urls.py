b665291b5fb9017ab562358c83e6e39ec4969cec
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-02 21:22:59 +0000

    Connect product_detail.html to url and products views

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 # Urls.py products file
 
 urlpatterns = [
-    path('', views.products_view, name='products')
+    path('', views.products_view, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Products, Category
 
 
+# For for individual products
+def product_detail(request, product_id):
+    product = get_object_or_404(Products, pk=product_id)
+    context = {
+        'product': product,
+        }
+    return render(request, 'products/product_detail.html', context)
+
+
+# Category view
 def categories(request):
     return {
         'categories': Category.objects.all()
