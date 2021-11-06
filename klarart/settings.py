ac86b1f92acd78598aece2ae11cc48c9a94307dc
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-06 16:58:15 +0000

    Deployment part 1

--- klarart/settings.py
+++ klarart/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,7 +103,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
+
+
 
 
 # Password validation
