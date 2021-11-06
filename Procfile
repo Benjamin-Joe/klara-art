eab4b7ba088671983ddf4496c6150c58a994a30d
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-06 17:15:08 +0000

    Deployment 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn klarart.wsgi:application
--- klarart/settings.py
+++ klarart/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure--09uqmml4y9@lp0%8w+-&s53ii-gtg-um6kzpvg8i2n)wj$%p0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['klarart-1.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -98,12 +98,18 @@ WSGI_APPLICATION = 'klarart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DASABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
- }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
